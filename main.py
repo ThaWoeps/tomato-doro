7e041482f3ac25e5beb21c5d8e4e64fa39c05885
ThaWoeps <dirkbouwhuis@hotmail.com>
2022-05-17 22:52:11 +0200

    adding sys arguments

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 #!/bin/python3
 
 import time as t
+import sys
 
-#asking user to give amount of seconds
-seconds = int(input("How many seconds to wait "))
+minutes = 30 # default is 30 min
+secondsInMinute = 60
 
-for i in range(seconds):
-    print (f"{seconds-i} ... seconds remaining \n")
-    t.sleep(1)
+def countDown(m):   
+    #calculate total seconds
+    totalSec = m*secondsInMinute
 
+    for i in range(totalSec):
+        print (f"{totalSec-i} ... seconds remaining \n")
+        t.sleep(1)
+
+#we ask only for minutes
+if len(sys.argv) < 0:
+    minutes = int(input("How many minutes ")) 
+if len(sys.argv) > 1:
+    minutes = int(sys.argv[1])
+
+
+
+countDown(minutes) #devide imput by seconds
 print(f"time is up")
+
+
+#todo
+# now it gives an ugly 1200 seconds remain for 20 min
+# turn this into a nice count down of 20:00
